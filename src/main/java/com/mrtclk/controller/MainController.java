6788cdbf486fe45d1b8fc5ad9c8a591c75e86a61
Murat Çelik <muratcelik11@gmail.com>
2021-11-09 01:52:23 +0300

    Update MainController.java

--- src/main/java/com/mrtclk/controller/MainController.java
+++ src/main/java/com/mrtclk/controller/MainController.java
@@ -1,14 +1,10 @@
 package com.mrtclk.controller;
 
-import com.mrtclk.model.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class MainController {
